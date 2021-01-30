0349cfa0623d8f7ea5792477b2f8d7693e3f04dd
Izzy Rosen <i.rosen@rosenrealm.com>
2021-01-30 14:48:14 -0800

    Flask form retrieval is set up

--- flaskyboi.py
+++ flaskyboi.py
@@ -13,6 +13,19 @@ def hello_world():
     return render_template('index.html')
 
 
+
+@app.route('/', methods=['post', 'get'])
+def getform():
+    message = ''
+    if request.method == 'POST':
+        quarter = request.form.get('quarter')  # access the data inside 
+        year = request.form.get('year')
+        department = request.form.get('department')
+        cnumber = request.form.get('course-number')
+        time = request.form.get('time')
+        message = f'Data Retrieved: {quarter},{year},{department},{cnumber},{time}'
+    return render_template('index.html', message=message)
+
 # start the server with the 'run()' method
 if __name__ == '__main__':
     app.run(debug=True)
