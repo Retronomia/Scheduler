d3f14cf70705d9059303a7bdb26ce62eddec35b1
Izzy Rosen <i.rosen@rosenrealm.com>
2021-01-30 14:38:56 -0800

    Reorganized files so css will work on flask

--- flaskyboi.py
+++ flaskyboi.py
@@ -1,11 +1,18 @@
 #testing out flask
 #no hate name plz is beautiful
 
-from flask import Flask, request
+from flask import Flask, request,render_template
+import os
 
-
-app = Flask(__name__)
+project_root = os.path.dirname(__file__)
+template_path = os.path.join(project_root, './')
+app = Flask(__name__, template_folder=template_path)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
