86f852fd1db681edd1d45f472950062aa8532863
Izzy Rosen <i.rosen@rosenrealm.com>
2021-01-30 17:09:01 -0800

    Can add, remove classes!

--- flaskyboi.py
+++ flaskyboi.py
@@ -3,10 +3,13 @@
 
 from flask import Flask, request,render_template
 import os
+import urllib
 
 project_root = os.path.dirname(__file__)
 template_path = os.path.join(project_root, './')
 app = Flask(__name__, template_folder=template_path)
+message = dict()
+
 
 @app.route('/')
 def hello_world():
@@ -16,14 +19,26 @@ def hello_world():
 
 @app.route('/', methods=['post', 'get'])
 def getform():
-    message = ''
     if request.method == 'POST':
-        quarter = request.form.get('quarter')  # access the data inside 
-        year = request.form.get('year')
-        department = request.form.get('department')
-        cnumber = request.form.get('course-number')
-        time = request.form.get('time')
-        message = f'Data Retrieved: {quarter},{year},{department},{cnumber},{time}'
+        if "form" in request.form:
+            if request.form["form"] == "const":
+                quarter = request.form.get('quarter')  # access the data inside 
+                year = request.form.get('year')
+                time = request.form.get('time')
+                message['quarter'] = quarter
+                message['year'] = year
+                message['time'] = time
+            elif request.form["form"] == "speccourse":
+                department = request.form.get('department')
+                cnumber = request.form.get('course-number')
+                if 'courses' in message:
+                    if (cnumber,department) not in message['courses']:
+                        message['courses'].append((cnumber,department))
+                else:
+                    message['courses'] = [(cnumber,department)]
+        elif 'course_rem' in request.form and 'courses' in message:
+            del message['courses'][int(request.form['course_rem'])]
+
     return render_template('index.html', message=message)
 
 # start the server with the 'run()' method
