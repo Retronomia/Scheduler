60b25f98141f5d576ef1cefb15d87100bffe98ca
Izzy Rosen <i.rosen@rosenrealm.com>
2021-01-30 20:43:41 -0800

    added table thingeh

--- flaskyboi.py
+++ flaskyboi.py
@@ -9,12 +9,7 @@ project_root = os.path.dirname(__file__)
 template_path = os.path.join(project_root, './')
 app = Flask(__name__, template_folder=template_path)
 message = dict()
-
-
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
-
+gen_courses = [{'I&C SCI 33': {'type': 'LEC', 'time': 'MWF   1:00- 1:50p', 'final': 'Wed, Mar 17, 1:30-3:30pm', 'status': 'OPEN', 'section': 'A', 'instructor': 'PATTIS, R.'}, 'I&C SCI 33 LAB': {'type': 'LAB', 'time': 'TuTh  2:00- 3:50p', 'final': '', 'status': 'OPEN', 'section': '1', 'instructor': 'CHIO, A.'}, 'I&C SCI 45': {'type': 'LEC', 'time': 'TuTh  5:00- 6:20p', 'final': 'Thu, Mar 18, 4:00-6:00pm', 'status': 'OPEN', 'section': 'A', 'instructor': 'IBRAHIM, M.'}, 'WRITING 39C': {'type': 'SEM', 'time': '*TBA*', 'final': '', 'status': 'OPEN', 'section': 'C', 'instructor': 'VENEGAS, Y.'}}, 3.8]
 
 
 @app.route('/', methods=['post', 'get'])
@@ -38,8 +33,10 @@ def getform():
                     message['courses'] = [(cnumber,department)]
         elif 'course_rem' in request.form and 'courses' in message:
             del message['courses'][int(request.form['course_rem'])]
+    return render_template('index.html', message=message,gen_courses=gen_courses)
 
-    return render_template('index.html', message=message)
+
+
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
