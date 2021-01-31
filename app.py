bccc6bdcdbc3206a1a58a4c61c91311c170678f3
Izzy Rosen <i.rosen@rosenrealm.com>
2021-01-31 14:34:00 -0800

    Fixed Table Bug

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from time_compare import TimeComparison
 from logic import get_optimal_time
 from websoc_data import get_data
 from datetime import datetime
+
 project_root = os.path.dirname(__file__)
 template_path = os.path.join(project_root, './')
 app = Flask(__name__, template_folder=template_path)
@@ -25,8 +26,6 @@ colors = ['color1','color2','color3','color4','color5','color6','color7','color8
 #['#DFFF00','#FFBF00','#FF7F50','#DE3163','#9FE2BF','#40E0D0','#6495ED','#CCCCFF']
 #time_increments= enumerate(time_increments)
 #gen_courses = [{'35570 ': {'name': 'I&C SCI 33', 'type': 'LEC', 'time': 'MWF   1:00- 1:50p', 'final': 'Wed, Mar 17, 1:30-3:30pm', 'status': 'OPEN', 'section': 'A', 'instructor': 'PATTIS, R.'}, '35581 ': {'name': 'I&C SCI 33', 'type': 'LAB', 'time': 'TuTh  2:00- 3:50p', 'final': '', 'status': 'OPEN', 'section': '1', 'instructor': 'CHIO, A.'}, '35600 ': {'name': 'I&C SCI 45', 'type': 'LEC', 'time': 'TuTh  5:00- 6:20p', 'final': 'Thu, Mar 18, 4:00-6:00pm', 'status': 'OPEN', 'section': 'A', 'instructor': 'IBRAHIM, M.'}, '33303 ': {'name': 'WRITING 39C', 'type': 'SEM', 'time': 'TBA', 'final': '', 'status': 'OPEN', 'section': 'C', 'instructor': 'VENEGAS, Y.'}}, 3.8]
-
-
 
 
 def page_not_found(e):
@@ -108,12 +107,27 @@ def make_courses():
 
 def make_table():
     global table_dict
+    dates = {'M':0,'Tu':1,'W':2,'Th':3,'F':4}
+    table = [[None for day in range(0,5)] for interval in range(0,90)]
+    for interval in range(0,90):
+        for course in times_list:
+            if (interval*10  == course[2]):
+                rows = [dates[val] for val in set(course[1])]
+                rowspan = ((course[3] - course[2])//10)
+                print(rows,rowspan)
+                for row in rows:
+                    table[interval][row] = (course,rowspan)
+                    if rowspan > 1:
+                        for v in range(interval+1,interval+rowspan):
+                            table[v][row] = 'REM'
+    table_dict = table
+
+    '''
     for interval in range(0,90):
         for course in times_list:
             if (interval*10  == course[2]):
                 table_dict[interval] = course 
-
-
+    '''
 
 
 
